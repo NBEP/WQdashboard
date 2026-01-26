65a82579d05de402f30e4f5d618157cc7d26ccf6
msorlien <47087329+msorlien@users.noreply.github.com>
2026-01-26 17:57:33 -0500

    functional map
    to do: map table

--- R/app_server.R
+++ R/app_server.R
@@ -7,10 +7,10 @@
 app_server <- function(input, output, session) {
   # Set variables
   map_bounds <- list(
-    lng1 = min(df_sites$Longitude),
     lat1 = min(df_sites$Latitude),
-    lng2 = max(df_sites$Longitude),
-    lat2 = max(df_sites$Latitude)
+    lat2 = max(df_sites$Latitude),
+    lng1 = min(df_sites$Longitude),
+    lng2 = max(df_sites$Longitude)
   )
 
   # Add module servers ----
@@ -27,17 +27,27 @@ app_server <- function(input, output, session) {
     })
   )
 
-  # map_var <- mod_map_server(
-  #   "map",
-  #   in_var = sidebar_var,
-  #   map_bounds = map_bounds,
-  #   df_raw = df_score[0, ],
-  #   selected_tab = reactive({
-  #     input$tabset
-  #   }),
-  #   shp_watershed = shp_watershed,
-  #   shp_river = shp_river
-  # )
+  watershed <- NULL
+  river <- NULL
+
+  if (exists("shp_watershed")) {
+    watershed <- shp_watershed
+  }
+  if (exists("shp_river")) {
+    river <- shp_river
+  }
+
+  map_var <- mod_map_server(
+    "map",
+    in_var = sidebar_var,
+    map_bounds = map_bounds,
+    df_raw = df_score[0, ],
+    selected_tab = reactive({
+      input$tabset
+    }),
+    shp_watershed = watershed,
+    shp_river = river
+  )
 
   # mod_report_card_server(
   #   "report_card_1",
