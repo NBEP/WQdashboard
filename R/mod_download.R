475e94fac73c0e90760db3c02bf3006e34688ae2
msorlien <47087329+msorlien@users.noreply.github.com>
2026-02-11 14:52:57 -0500

    overhauled download module
    dropped excel format because it struggled with non-utf8 characters

--- R/mod_download.R
+++ R/mod_download.R
@@ -13,18 +13,9 @@ mod_download_ui <- function(id) {
     bslib::card(
       h2("Suggested Citation"),
       textOutput(ns("citation")),
-      h2("Download Data"),
-      importwqd::dropdown(
-        id = ns("dl_format"),
-        label = "Select Format",
-        choices = c("xls", "csv", "tsv"),
-        choice_names = c("Excel", "csv", "tsv"),
-        sorted = FALSE,
-        multiple = FALSE
-      ),
       downloadButton(
         ns("dl"),
-        "Download",
+        "Download (csv)",
         style = "width:fit-content"
       )
     )
@@ -46,38 +37,64 @@ mod_download_server <- function(
       gsub("DATE", format(Sys.time(), "%B %d, %Y"), cit)
     })
 
-    # Filter data ----
-    df_sites_filter <- reactive({
-      req(selected_var$sites_all)
+    # Download ----
+    output$dl <- downloadHandler(
+      filename = function() {"water_quality.zip"},
 
-      site_list <- selected_var$sites_all()
+      content = function(file) {
+        # Download progress notification
+        id <- showNotification(
+          "Downloading data...",
+          duration = NULL,
+          closeButton = FALSE
+        )
+        on.exit(removeNotification(id), add = TRUE)
 
-      dplyr::filter(sites, .data$Site_ID %in% !!site_list)
-    })
+        # Set temp directory
+        temp_dir <- file.path(tempdir(), as.integer(Sys.time()))
+        dir.create(temp_dir)
 
-    df_data_filter <- reactive({
-      req(selected_var$sites_all)
-      req(selected_var$param_all())
-      req(selected_var$year_all())
+        # Filter data
+        site_list <- selected_var$sites_all()
+        param <- selected_var$param_all()
+        year <- selected_var$year_all()
+        depth_list <- c(NA, selected_var$depth_all())
 
-      site_list <- selected_var$sites_all()
-      param <- selected_var$param_all()
-      year <- selected_var$year_all()
-      depth_list <- c(NA, selected_var$depth_all())
+        sites_filter <- sites |>
+          dplyr::filter(.data$Site_ID %in% !!site_list) |>
+          importwqd:::prep_pdf(na_sub="")
 
-      dat <- results |>
-        dplyr::filter(
-          .data$Site_ID %in% !!site_list,
-          .data$Parameter %in% !!param,
-          .data$Year %in% !!year
-        ) |>
-        dplyr::select(!"Year")
+        results_filter <- results |>
+          dplyr::filter(
+            .data$Site_ID %in% !!site_list,
+            .data$Parameter %in% !!param,
+            .data$Year %in% !!year
+          ) |>
+          dplyr::select(!"Year")
 
-      if (isTruthy(depth_list)) {
-        dat <- dplyr::filter(dat, .data[["Depth Category"]] %in% !!depth_list)
+        if (isTruthy(depth_list)) {
+          results_filter <- results_filter |>
+            dplyr::filter(.data$Depth_Category %in% !!depth_list)
+        }
+
+        results_filter <- results_filter |>
+          importwqd:::prep_pdf(na_sub="")
+
+        # Create file
+        temp_sites <- paste0(temp_dir, "/site_metadata.csv")
+        temp_results <- paste0(temp_dir, "/result_data.csv")
+
+        readr::write_excel_csv(sites_filter, temp_sites)
+        readr::write_excel_csv(results_filter, temp_results)
+
+        zip::zip(
+          zipfile = file,
+          files = dir(temp_dir),
+          root = temp_dir
+        )
       }
+    )
 
-      dat
-    })
+
   })
 }
