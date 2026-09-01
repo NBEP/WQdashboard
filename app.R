1b12e801af79728cb3a3bf00c11fcaf414e172ab
msorlien <47087329+msorlien@users.noreply.github.com>
2026-08-31 21:58:28 -0400

    attempted bug fix

--- app.R
+++ app.R
@@ -1,7 +1,4 @@
-# Launch the ShinyApp (Do not remove this comment)
-# To deploy, run: rsconnect::deployApp()
-# Or use the blue button on top of this file
+# Launch the ShinyApp
 
-pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-options("golem.app.prod" = TRUE)
-WQdashboard::run_app() # add parameters here (if any)
+pkgload::load_all(export_all = FALSE, helpers = FALSE, attach_testthat = FALSE)
+run_app() # add parameters here (if any)
